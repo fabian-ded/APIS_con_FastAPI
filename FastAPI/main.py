083569ace34d77148d414f1d40157a7872436b9a
favian silva <fabiansil902@gmail.com>
2025-05-17 11:16:30 -0500

    esta vez vi como usar los archivos estaticos es decir como mostrar una imagen

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -8,6 +8,7 @@
 
 from fastapi import FastAPI
 from routers import productos, users
+from fastapi.staticfiles import StaticFiles #aqui se deben colocar la biblioteca para utilizar los archivos estaticos
 
 #iniciar servidor
 app = FastAPI()
@@ -15,6 +16,9 @@ app = FastAPI()
 #rutas del servidor
 app.include_router(productos.router)
 app.include_router(users.router)
+
+#rutas estaticas
+app.mount("/static", StaticFiles(directory="static"), name="static")#esta linea es para hacer funcionar las imagenes, es decir archivos estaticos
 
 @app.get("/")
 async def root(): #muy improtante es tener claro como se va a llamar cada funcion para no ser confundido

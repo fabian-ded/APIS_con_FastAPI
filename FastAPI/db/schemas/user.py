54f6a8fa8add76a9aab56e9bbeb16d7282fe008d
favian silva <fabiansil902@gmail.com>
2025-05-26 15:27:00 -0500

    sigo con el cusro esta vez estuvimos viendo como se creaban los datos y se guardaban en mongoDB y ademas como hacer un get es decir como buscar esos datos en mongoDB pero con los id que se generaron automaticamente por la base de datos

--- FastAPI/db/schemas/user.py
+++ FastAPI/db/schemas/user.py
@@ -1,7 +1,10 @@
 #practicamente este esquema es el que se encarga de traer los datos que mongoDB ya guardo y los trae y hace la logica para que FastAPI lo comprenda
 
-def user_schema(user) -> dict:
+def user_schema(user) -> dict: #aqui estamos diciendo que nos va a retorna un diccionario
     return {"id": str(user["_id"]), #MongoDB guarda el ID con el nombre "_id", y es un tipo especial (ObjectId) que FastAPI no entiende directamente por eso
                                     #Aquí lo convertimos en un texto (str(...)) y lo llamamos simplemente id, para que lo entiensa fastAPI
             "username": user["username"],
             "email": user["email"]}
+    
+def users_schema(users) -> list: #aqui estamos diciendo que nos va a retorna una lista
+    return [user_schema(user) for user in users]
--- FastAPI/routers/usersdb.py
+++ FastAPI/routers/usersdb.py
@@ -3,7 +3,8 @@
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User #para importar la clase que se creo de "User" para la base de datos los datos que debe tener cada usuario
 from db.cliente import db_client #se importa la base de datos
-from db.schemas.user import user_schema # se importa el archivo que nos va extraer los datos que venga de la base de datos de mongoDB
+from db.schemas.user import user_schema, users_schema # se importa el archivo que nos va extraer los datos que venga de la base de datos de mongoDB
+from bson import ObjectId
 
 router = APIRouter(prefix="/userdb",
                    tags=["userdb"])#El "prefix" es una funcion que nos permite colocar la ruta establecida para todo el crud que se haga aqui
@@ -12,35 +13,20 @@ router = APIRouter(prefix="/userdb",
 users_list=[()]
 
 
-@router.get("/")#aqui se llama desde la url para que se muestren los datos de los usuarios
-async def Users():
-    return users_list #aqui se muestra la lista de los datos de los usuarios
+@router.get("/", response_model=list[User])#aqui se llama desde la url para que se muestren los datos de los usuarios
+async def users():
+    #aqui se muestra la lista de los datos de los usuarios
+    return users_schema(db_client.local.users.find())#con el find() estamos pidiendo toda la informacion que tengamos en la base de datos
 
 #ejemplo mio, para llamar mediante el id "path"
 @router.get("/{id}")
-async def user(id:int):
-    return users_list[id]
-
-#ejemplo del curso para llamar mediante el id "path"
-@router.get("/{id}")
-async def user(id:int):
-    users = filter(lambda user: user.id == id, users_list)
-    try:
-        return list(users)[0]
-    except:
-        raise HTTPException(status_code=404, detail="no se ha encontrado el usuario")
+async def user(id:str):
+    return Buscar("_id", ObjectId(id))
 
 #ejemplo del curso query
 @router.get("/")    
-async def user(id:int):
-    return Buscar(id)#aqui se llama la funcion que se creo para que haga la logica de comprar los id con la url pasada
-    
-def Buscar(id: int): #esta es una manera practica para no tener todo en el mismo http y asi es una manera facil de hacerlo y con menos errores  
-    users = filter(lambda user: user.id == id, users_list)
-    try:
-        return list(users)[0]#ese "[0]" que se ve ahi significa que buscara el primer resultado de la busqueda que tenga parecido
-    except:
-        raise HTTPException(status_code=404, detail="no se ha encontrado el usuario")
+async def user(id:str):
+    return Buscar("_id", ObjectId(id))#aqui se llama la funcion que se creo para que haga la logica de comprar los id con la url pasada
 
 #ejemplo mio
 @router.get("/")
@@ -50,11 +36,42 @@ async def user(id:int, name:str):
         return list(users)[0]
     except:
         return {"error":"no se ha encontrado el usuario"}
-    
+
+
+
+#ejemplo en la clase
+#def Buscar_email(email: str): #esta es una manera practica para no tener todo en el mismo http y asi es una manera facil de hacerlo y con menos errores  
+    #try:
+    #    user = db_client.local.users.find_one({"email": email})#aqui buscamos el email que ingreso el usuario y el mail que tenemos en la base de datos
+    #    if user is not None:#si user no esta es decir esta bacio se retorna lo de abajo
+    #        return User(**user_schema(user))#aqui se retorna la informacion desempaquetada es decir un json limpio
+    #except:
+        #raise HTTPException(status_code=404, detail="no se ha encontrado el usuario")
+
+# @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
+# async def user(user: User): #aqui se pasa el usuario a crear
+#     if type(Buscar(user.email)) == User: #aqui se busca si el email existe, si existe no se creara y lanza un error
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST, detail="el usuario ya existe")
+
+#     user_dict = dict(user)#aqui dict(user) convierte el objeto en un diccionario (porque Mongo no guarda objetos Pydantic).
+#     del user_dict["id"]#aqui borramos "id" porque cuando se crea un usuario nuevo, Mongo genera su propio _id automáticamente.
+
+#     id = db_client.local.users.insert_one(user_dict).inserted_id #aqui Se guarda el usuario en la base de datos y con inserted_id devuelve el ID (ObjectId) que Mongo le puso.
+# #insert_one es para crear
+#     new_user = user_schema(db_client.local.users.find_one({"_id" : id}))#aqui se esta recuperando la informacion del usuario mediante el "_id" y el "id", que anteriormente mongoDB ya nos habia
+#     #dado su "_id" creado por el y se paso a la variable de "id" por eso se hace la comparacion
+# #find_one es para comparar
+#     return User(**new_user)#las dos "**" se utilizan para desempaquetar los archivos json y los transforma a argumentos de (id="4")
+
+
+
+
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User): #aqui se pasa el usuario a crear
-    #if type(Buscar(user.id)) == User: #aqui se busca si el usuario existe, si existe no se creara y se busca mediante el id
-        #raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="el usuario ya existe")
+    if type(Buscar("email",user.email)) == User: #aqui se busca si el email existe, si existe no se creara y lanza un error, "email" es field y "user.email" es key, en la funcion de buscar 
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="el usuario ya existe")
 
     user_dict = dict(user)#aqui dict(user) convierte el objeto en un diccionario (porque Mongo no guarda objetos Pydantic).
     del user_dict["id"]#aqui borramos "id" porque cuando se crea un usuario nuevo, Mongo genera su propio _id automáticamente.
@@ -95,3 +112,12 @@ async def user(id:int):
         raise HTTPException (status_code=status.HTTP_401_UNAUTHORIZED, detail="no se ha encontrado el usuario")
     else:
         return {"message": f"Usuario con id {id} eliminado correctamente"} #Respuesta de éxito
+    
+def Buscar(field: str, key): #field lo que se quiere buscar, key donde se quiere encontrar o buscar
+    try:
+        user = db_client.local.users.find_one({field: key})#aqui estamos haciendo es que "field" es el criterio de busqueda y "key" es la clave por la cual quiero buscar
+        #es decir que si quiero buscar el nombre"field" en la base de datos, pues se pasa que en donde quieres que busque "key" ejemplo "nombre(field)==nombre(key)"  
+        if user is not None:#si user no esta es decir esta bacio se retorna lo de abajo
+            return User(**user_schema(user))#aqui se retorna la informacion desempaquetada es decir un json limpio
+    except:
+        raise HTTPException(status_code=404, detail="no se ha encontrado el usuario")
